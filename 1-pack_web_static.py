5a6b53370baab888d55ac0a54be2ef28221de559
HeimerR <ing.heimer.rojas@gmail.com>
2019-08-19 20:22:10 +0000

    [feature] create tgz file from web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from fabric.api import run, local, put
+import datetime
+
+
+def do_pack():
+    now = datetime.datetime.now()
+    date = (str(now.year) + str(now.month) + str(now.day) + str(now.hour) +
+            str(now.minute) + str(now.second))
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz ./web_static".format(date))
+        return "/versions/web_static_{}".format(date)
+    except:
+        return None
