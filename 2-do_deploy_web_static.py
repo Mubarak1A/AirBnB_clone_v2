5139ccaab36108efda2595253856daf196042920
heimerr <732@holbertonschool.com>
2019-08-19 22:28:30 +0000

    [feature] script that deploys static files

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+from fabric.api import run, local, put, env
+import datetime
+import os
+
+
+def do_pack():
+    now = datetime.datetime.now()
+    date = (str(now.year) + str(now.month) + str(now.day) + str(now.hour) +
+            str(now.minute) + str(now.second))
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz ./web_static".format(date))
+        return "/versions/web_static_{}".format(date)
+    except:
+        return None
+
+
+env.hosts = ['35.190.188.208', '35.227.47.184']
+
+
+def do_deploy(archive_path):
+    if os.path.exists(archive_path) is False:
+        return False
+    try:
+        upload = put(archive_path, "/tmp/")
+        name = archive_path[9:-4]
+        rmt_path = "/data/web_static/releases/" + name
+        run("mkdir {}".format(rmt_path))
+        run("tar -xvzf /tmp/{}.tgz --directory {}/".format(name, rmt_path))
+        run("rm /tmp/{}.tgz".format(name))
+        run("rm /data/web_static/current")
+        run("ln -nsf /data/web_static/releases/{} /data/web_static/current"
+            .format(name))
+        run("mv {}/web_static/* {}".format(rmt_path, rmt_path))
+        run("rm -d {}/web_static/".format(rmt_path))
+        return True
+    except:
+        return False
