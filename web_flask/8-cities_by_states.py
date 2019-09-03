0eab433a7690a0b86e343828d5eea4a4b3d17b7c
heimerr <732@holbertonshool.com>
2019-09-03 15:17:10 +0000

    [feature] list states and cities on the same website

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""starts a Flask web application """
+from flask import Flask
+from flask import render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def index():
+    my_dict = storage.all()
+    return render_template('8-cities_by_states.html', mydict=my_dict)
+
+app.run(host='0.0.0.0')
+
+
+@app.teardown_appcontext
+def teardown():
+    storage.close()
