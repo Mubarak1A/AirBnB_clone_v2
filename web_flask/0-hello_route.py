944c8156ae7003368cbdd9bb11ce55c937102935
heimerr <732@holbertonschool.com>
2019-09-03 17:51:01 +0000

    [fix] __name__ == __main__ condition added

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ starts a Flask web application """
-from flask import Flask
-
-app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """ dummy start function """
-    return "Hello HBNB!"
-app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    from flask import Flask
+
+    app = Flask(__name__)
+
+    @app.route('/', strict_slashes=False)
+    def index():
+        """ dummy start function """
+        return "Hello HBNB!"
+    app.run(host='0.0.0.0')
