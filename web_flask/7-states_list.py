6246369c63c1e513ef7b0ae602be523d6d35624d
heimerr <732@holbertonschool.com>
2019-09-03 21:18:34 +0000

    [fix] rollback storage definition

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
     from models import storage
 
     app = Flask(__name__)
-    my_dict = storage.all('State')
 
     @app.route('/states_list', strict_slashes=False)
     def index():
+        my_dict = storage.all('State')
         return render_template('7-states_list.html', mydict=my_dict)
 
     app.run(host='0.0.0.0')
