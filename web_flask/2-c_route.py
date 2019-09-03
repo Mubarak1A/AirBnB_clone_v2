c35a13a8e2788df357641b65c43166d2015e94a1
heimerr <732@holbertonschool.com>
2019-09-03 18:03:44 +0000

    [fix] name eq main condition added

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """starts a Flask web application """
-from flask import Flask
-
-app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    return "Hello HBNB!"
+if __name__ == "__main__":
+    from flask import Flask
 
+    app = Flask(__name__)
 
-@app.route('/hbnb', strict_slashes=False)
-def index2():
-    return "HBNB"
+    @app.route('/', strict_slashes=False)
+    def index():
+        return "Hello HBNB!"
 
+    @app.route('/hbnb', strict_slashes=False)
+    def index2():
+        return "HBNB"
 
-@app.route('/c/<text>', strict_slashes=False)
-def index3(text):
-    return "C {}".format(text).replace("_", " ")
-app.run(host='0.0.0.0')
+    @app.route('/c/<text>', strict_slashes=False)
+    def index3(text):
+        return "C {}".format(text).replace("_", " ")
+    app.run(host='0.0.0.0')
