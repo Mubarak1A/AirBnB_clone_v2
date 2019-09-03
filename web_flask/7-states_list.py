eedf33840cae789c022d8cb6f91a2bc94b8e9ebf
heimerr <732@holbertonschool.com>
2019-09-03 18:17:17 +0000

    [fix] name eq main condition added

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """starts a Flask web application """
-from flask import Flask
-from flask import render_template
-from models import storage
 
 
-app = Flask(__name__)
+if __name__ == "__main__":
+    from flask import Flask
+    from flask import render_template
+    from models import storage
 
+    app = Flask(__name__)
 
-@app.route('/states_list', strict_slashes=False)
-def index():
-    my_dict = storage.all()
-    return render_template('7-states_list.html', mydict=my_dict)
+    @app.route('/states_list', strict_slashes=False)
+    def index():
+        my_dict = storage.all()
+        return render_template('7-states_list.html', mydict=my_dict)
 
-app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
 
-
-@app.teardown_appcontext
-def teardown():
-    storage.close()
+    @app.teardown_appcontext
+    def teardown():
+        storage.close()
