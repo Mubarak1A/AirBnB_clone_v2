6e8e6dae37b0ade91a108f7f710ad8f0416cd7a0
heimerr <732@holbertonschool.com>
2019-09-03 21:44:46 +0000

    [fix] if name eq main at the bottom

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """starts a Flask web application """
+from flask import Flask
+from flask import render_template
+from models import storage
 
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def index():
+    my_dict = storage.all('State')
+    return render_template('7-states_list.html', mydict=my_dict)
+
+
+@app.teardown_appcontext
+def teardown(tmp):
+    storage.close()
 
 if __name__ == "__main__":
-    from flask import Flask
-    from flask import render_template
-    from models import storage
-
-    app = Flask(__name__)
-
-    @app.route('/states_list', strict_slashes=False)
-    def index():
-        my_dict = storage.all('State')
-        return render_template('7-states_list.html', mydict=my_dict)
-
     app.run(host='0.0.0.0')
-
-    @app.teardown_appcontext
-    def teardown(tmp):
-        storage.close()
