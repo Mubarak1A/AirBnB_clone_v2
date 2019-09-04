976b52f3ca24b3953da212b579433cba7971787a
heimerr <732@holbertonschool.com>
2019-09-04 20:02:54 +0000

    [feature] load states, cities, places on the web static

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -8,12 +8,15 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def index():
     state_dict = storage.all('State')
     amenity_dict = storage.all('Amenity')
-    return render_template('10-hbnb_filters.html', state_dict=state_dict,
-                           amenity_dict=amenity_dict)
+    place_dict = storage.all('Place')
+    user_dict = storage.all('User')
+    return render_template('100-hbnb.html', state_dict=state_dict,
+                           amenity_dict=amenity_dict, place_dict=place_dict,
+                           user_dict= user_dict)
 
 
 @app.teardown_appcontext
