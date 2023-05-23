529c9090b3255717ea67b696188f5f58aeef5f7f
Adesina Mubarak <adesinamubarak123@gmail.com>
2023-05-23 14:43:05 +0100

    add documentation

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+''' Fabric script that generates a .tgz archive from the contents of the web_static folder
+    using the function do_pack
+'''
 from fabric.api import run, local, put
 import datetime
 
 
 def do_pack():
+    '''generates a .tgz archive from the contents of the web_static folder'''
     now = datetime.datetime.now()
     date = (str(now.year) + str(now.month) + str(now.day) + str(now.hour) +
             str(now.minute) + str(now.second))
